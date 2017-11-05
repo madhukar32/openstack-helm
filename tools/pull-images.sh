0dcd21d63c8a7c420daf8a0aadb1448329cbb043
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-05 13:41:19 +0300

    fix pulling images

--- tools/pull-images.sh
+++ tools/pull-images.sh
@@ -9,7 +9,7 @@ fi
 
 for CHART_DIR in ${CHART_DIRS} ; do
   if [ -e ${CHART_DIR}values.yaml ]; then
-    for IMAGE in $(cat ${CHART_DIR}values.yaml | yq '.images.tags | map(.) | join(" ")' | tr -d '"'); do
+    for IMAGE in `cat ${CHART_DIR}values.yaml | yq '.images.tags | map(.) | join(" ")' | tr -d '"'`; do
       sudo docker inspect $IMAGE >/dev/null|| sudo docker pull $IMAGE
     done
   fi
