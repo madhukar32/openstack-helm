68ec0cec8b6af021ea38ed42b1d2ff30ac0ed2dc
Andrey Pavlov <andrey.mp@gmail.com>
2017-12-03 11:10:26 +0300

    pull specific images before helming
    Change-Id: I01310b9c04be4fce66e35682856afee6382b2893

--- tools/pull-images.sh
+++ tools/pull-images.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-exit 0
-
 set -x
-
-if [ "x$1" == "x" ]; then
-  CHART_DIRS="$(echo ./*/)"
-else
-  CHART_DIRS="$(echo ./$1/)"
-fi
-
-for CHART_DIR in ${CHART_DIRS} ; do
-  if [ -e ${CHART_DIR}values.yaml ]; then
-    for IMAGE in $(cat ${CHART_DIR}values.yaml | yq '.images.tags | map(.) | join(" ")' | tr -d '"'); do
-      docker inspect $IMAGE >/dev/null|| docker pull $IMAGE
+for CHART_DIR in ceph dns-helper etcd ingress ldap mariadb memcached rabbitmq keystone glance libvirt nova neutron; do
+  if [ -e ${CHART_DIR}/values.yaml ]; then
+    for IMAGE in $(cat ${CHART_DIR}/values.yaml | yq '.images.tags | map(.) | join(" ")' | tr -d '"'); do
+      sudo docker inspect $IMAGE >/dev/null|| sudo docker pull $IMAGE
     done
   fi
 done
